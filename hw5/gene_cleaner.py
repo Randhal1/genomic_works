7b18304a75f50e0004cb2e8ffabe3a6c91874129
Randhal Ramirez <randhal45@gmail.com>
2025-10-01 11:08:58 -0600

    HW5 Basis in Linux -Process mac pending-

---
+++ hw5/gene_cleaner.py
@@ -0,0 +1,14 @@
+#!/home/randhal/proj/genomic_env/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Drop the duplicates of the AML genes file
+def clean_genes(data):
+    df = pd.read_csv(data)
+    aa = df["Gene"].drop_duplicates()
+    aa.to_csv("AML_gene_clean.csv", index=False)
+
+
+if __name__=='__main__':
+    clean_genes("AML_gene.csv")
