6946332accc2607718bf7698be41374eafb4f6a1
Randhal <randhal45@gmail.com>
2025-10-07 16:45:30 -0600

    Regular comment previous to task 6.

--- hw5/gene_cleaner.py
+++ hw5/gene_cleaner.py
@@ -1,4 +1,4 @@
-#!/home/randhal/proj/genomic_env/bin/python
+#!/Users/randhal/Documents/Interpreters/xptopics/bin/python
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -6,7 +6,7 @@ import pandas as pd
 # Drop the duplicates of the AML genes file
 def clean_genes(data):
     df = pd.read_csv(data)
-    aa = df["Gene"].drop_duplicates()
+    aa = df["SYMBOL"].drop_duplicates()
     aa.to_csv("AML_gene_clean.csv", index=False)
 
 
